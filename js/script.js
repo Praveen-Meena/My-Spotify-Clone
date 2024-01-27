471ed25f40886a543210498da4e8e29ee72b7591
Praveen-Meena <singhjimeena123@gmail.com>
2024-01-27 19:41:03 +0530

    final changes

--- js/script.js
+++ js/script.js
@@ -22,8 +22,8 @@ function secondsToMinutesAndSeconds(seconds) {
 async function getSongs(folder)
 {   
     currFolder = folder; 
-    let a = await fetch(`http://127.0.0.1:5500/${folder}/`);   // this we used when run this site locally
-    // let a = await fetch(`/${folder}/`); 
+    // let a = await fetch(`http://127.0.0.1:5500/${folder}/`);   // this we used when run this site locally
+    let a = await fetch(`/${folder}/`); 
     let response = await a.text(); 
     // console.log(response);
     let div = document.createElement("div"); 
@@ -81,8 +81,8 @@ const playMusic = (track, pause=false)=>{
 }
 
 async function displayAlbums(){
-    let a = await fetch(`http://127.0.0.1:5500/songs/`);   // this we used when run this site locally
-    // let a = await fetch(`/songs/`);       
+    // let a = await fetch(`http://127.0.0.1:5500/songs/`);   // this we used when run this site locally
+    let a = await fetch(`/songs/`);       
     let response = await a.text(); 
     let div = document.createElement("div"); 
     div.innerHTML = response; 
@@ -98,8 +98,8 @@ async function displayAlbums(){
         if(e.href.includes("/songs/")){
             let folder = e.href.split("/").slice(-1)[0]; 
             // Get the metadata of the folder
-            let a = await fetch(`http://127.0.0.1:5500/songs/${folder}/info.json`);      //this we used when run this site locally
-            // let a = await fetch(`/songs/${folder}/info.json`);
+            // let a = await fetch(`http://127.0.0.1:5500/songs/${folder}/info.json`);      //this we used when run this site locally
+            let a = await fetch(`/songs/${folder}/info.json`);
             let response = await a.json(); 
             cardContainer.innerHTML = cardContainer.innerHTML + 
                                                                 `<div data-folder="${folder}" class="card">
